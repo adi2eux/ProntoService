71524cdad7c3945370f12534ff71cb7b5ef3bf09
Aditi <gaditi2001@gmail.com>
2023-02-25 06:31:27 +0530

    uploaded

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer=()=>{
+    return (
+        <>
+        <footer className="bg-light text-center">
+            <p>
+                Website made by Aditi Ghosh.
+            </p>
+
+        </footer>
+
+        </>
+    );
+};
+
+export default Footer;
